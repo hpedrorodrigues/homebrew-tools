735a7440b5b738c5f5a30e7656ed0d308433d79e
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2020-12-03 14:54:28 -0300

    Updating sphynx

--- Formula/sphynx.rb
+++ Formula/sphynx.rb
@@ -1,13 +1,14 @@
 class Sphynx < Formula
   desc "Command-line tool to help you with your day to day tasks"
   homepage "https://github.com/hpedrorodrigues/sphynx"
-  head "https://github.com/hpedrorodrigues/sphynx.git"
+  url "https://github.com/hpedrorodrigues/sphynx/archive/v0.1.0.tar.gz"
+  sha256 "0f3cd7da6f0862552f4aa429c0b626f54c94a8a26f84e83e404ea13f1bc41651"
 
   def install
     bin.install "sx"
     prefix.install "cli"
 
-    ["lint", "fmt"].each { | cmd |
+    ["lint", "fmt", "test"].each { | cmd |
       rm "#{prefix}/cli/#{cmd}"
     }
     ["dotfiles", "playbook"].each { | namespace |
@@ -18,12 +19,12 @@ class Sphynx < Formula
       s.gsub! /SPHYNX_DIR=.*/, "SPHYNX_DIR=\"#{prefix}\""
     end
 
-    ["sx.bash", "sx.zsh", "complete"].each { | completion |
+    ["sx.bash", "_sx", "complete"].each { | completion |
       inreplace "#{prefix}/cli/.internal/completion/#{completion}", "${SPHYNX_DIR:-}", "#{prefix}"
     }
 
     bash_completion.install "#{prefix}/cli/.internal/completion/sx.bash" => "sx"
-    zsh_completion.install "#{prefix}/cli/.internal/completion/sx.zsh" => "_sx"
+    zsh_completion.install "#{prefix}/cli/.internal/completion/_sx" => "_sx"
   end
 
   test do
