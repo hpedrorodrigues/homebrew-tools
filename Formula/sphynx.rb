7835b492002f9b996eb84293f381c33f8d4573e4
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2022-03-18 00:02:27 -0300

    Updating Sphynx to 0.1.19

--- Formula/sphynx.rb
+++ Formula/sphynx.rb
@@ -1,8 +1,8 @@
 class Sphynx < Formula
   desc "Command-line tool to help you with your day to day tasks"
   homepage "https://github.com/hpedrorodrigues/sphynx"
-  url "https://github.com/hpedrorodrigues/sphynx/archive/v0.1.18.tar.gz"
-  sha256 "83f0479e86ca9ac05e4f418a28903127e5459945450eac1d5fc9abf53f3757ac"
+  url "https://github.com/hpedrorodrigues/sphynx/archive/v0.1.19.tar.gz"
+  sha256 "2d7bc3d67b6611216f8e8f1d62820fe8a771231c856cf401d1c12f39b99c15f9"
 
   def install
     bin.install "sx"
